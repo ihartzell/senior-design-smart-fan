a5a0746fbcfc486b6cf1494c1b64c5331e76e227
zachary.sluder@gmail.com <zachary.sluder@gmail.com>
2019-10-22 09:48:49 -0400

    init

---
+++ motion.py
@@ -0,0 +1,10 @@
+from gpiozero import MotionSensor
+
+pir = MotionSensor(27)
+
+i=0
+while True:
+    pir.wait_for_motion()
+    print("{} You moved".format(i))
+#   pir.wait_for_no_motion()
+    i += 1
