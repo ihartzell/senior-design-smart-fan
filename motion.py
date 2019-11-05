97c1b54972920f88cc3832490f43cc6ca66e777a
zachary.sluder@gmail.com <zachary.sluder@gmail.com>
2019-11-05 11:07:15 -0500

    Added Code

--- motion.py
+++ motion.py
@@ -6,5 +6,5 @@ i=0
 while True:
     pir.wait_for_motion()
     print("{} You moved".format(i))
-#   pir.wait_for_no_motion()
+    #pir.wait_for_no_motion()
     i += 1
--- relay.py
+++ relay.py
@@ -1,6 +1,10 @@
 import RPi.GPIO as GPIO
 import os
-import time
+import json
+import requests
+from gpiozero import MotionSensor
+from threading import Thread
+from datetime import datetime 
 
 class FanController:
     PINS = {
@@ -10,24 +14,28 @@ class FanController:
     }
     
     def __init__(self):
+        self.is_on = False
+        self.last_speed = ''
+        
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
     
         GPIO.setup(self.PINS.values(), GPIO.OUT)
         GPIO.output(self.PINS.values(), GPIO.HIGH)
         
-    def on(self, speed):
+    def on(self, speed='low'):
         self.off()
-        
+        self.is_on = True
+        self.last_speed = speed
+    
         GPIO.output(self.PINS[speed], GPIO.LOW)
         
     def off(self):
-        GPIO.output(self.PINS.values(), GPIO.HIGH)
+        if self.is_on:
+            GPIO.output(self.PINS.values(), GPIO.HIGH)
         
 class TempController:
-    base_dir = '/sys/bus/w1/devices/'
-    device_id = '28-00000a9df21a'
-    device_file = base_dir + device_id + '/w1_slave'
+    DEVICE_FILE = '/sys/bus/w1/devices/28-00000a9df21a/w1_slave'
     
     def __init__(self):
         os.system('modprobe w1-gpio')
@@ -47,27 +55,90 @@ class TempController:
             
             return temp_c
         
-        
     def read_temp_raw(self):
-        f = open(self.device_file, 'r')
+        f = open(self.DEVICE_FILE, 'r')
         lines = f.readlines()
         f.close()
         
         return lines
- 
-if __name__ == '__main__':
-    fan_controller = FanController()
-    temp_controller = TempController()
     
+class MotionController:
+    PIN = 27
+    
+    def __init__(self):
+        self.sensor = MotionSensor(self.PIN)
+        self.last_motion = datetime.now() 
+        
+        Thread(target=self.record_motion).start()
+        
+    def record_motion(self):
+        while True:
+            self.sensor.wait_for_motion()
+            self.last_motion = datetime.now() 
+    
+class ApiController:
+    def __init__(self):
+        self.url_base = 'http://tp-api.zsluder.com/api'
+        self.headers = {
+            'Content-Type': 'application/json'
+        }
+        
+    def save_last_temp(self, temp):
+        response = requests.post(self.url_base + '/temperature', headers=self.headers)        
+
+        if response.status_code == 200:
+            return json.loads(response.content.decode('utf-8'))
+        else:
+            return None
+        
+    def get_temp_ranges(self):
+        response = requests.get(self.url_base + '/temperature/ranges', headers=self.headers)        
+
+        if response.status_code == 200:
+            return json.loads(response.content.decode('utf-8'))
+        else:
+            return None
+    
+def main():
     fan_controller.off()
-        
+    fan_controller.on('medium')
+    
+    last_time = datetime.now()
+
     while True:
-        temp = temp_controller.read()
-        print(temp)
+        # Motion detection
+        time_elapsed = (datetime.now() - motion_controller.last_motion).seconds
         
-        if temp >= 24:
+        print('No motion in {} sec'.format(time_elapsed))
+        
+        if int(time_elapsed) >= 15:
+            fan_controller.off()
+        else:
+            fan_controller.on(fan_controller.last_speed)
+            
+        # Temperature detection
+        temp = temp_controller.read()
+        time_elapsed = (datetime.now() - last_time).seconds
+        
+        if int(time_elapsed) >= 5:
+           api_controller.save_last_temp(temp)
+           last_time = datetime.now()
+        
+        '''if temp >= 24:
             fan_controller.on('high')
         elif temp >= 23 and temp < 24:
             fan_controller.on('medium')
         else:
-            fan_controller.on('low')
+            fan_controller.on('low')'''
+
+if __name__ == '__main__':
+    fan_controller = FanController()
+    temp_controller = TempController()
+    api_controller = ApiController()
+    motion_controller = MotionController()
+    
+    try: 
+        main()
+    except KeyboardInterrupt:
+        fan_controller.off()
+        
